4cdb37772617bdaeef134ab7d35eb034577000ef
peMayoral <pedromayoral@proton.me>
2024-09-08 18:42:44 -0300

    Added Player mechanics

--- main.py
+++ main.py
@@ -1,20 +1,41 @@
 import pygame
 from constants import *
+from player import Player
 
 
 def main():
+
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+
+
     pygame.init()
+
+    dt = 0
+    clock = pygame.time.Clock()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH/(2),SCREEN_HEIGHT/2)
+
+#GAMELOOP
 
     while(True):
+        #Allows quitting
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
+        #Background Color
         screen.fill(0)
+        player.draw(screen)
+        player.update(dt)
+
+        #Render display
         pygame.display.flip()
+        dt = clock.tick(60)/1000
+
+
+
 
 if __name__ == "__main__":
     main()
